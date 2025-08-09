f3077deaa121202e52fafce15bc7f6408b80388d
majid nazari <majidnazarister@gmail.com>
2025-08-09 12:35:41 +0330

    remove trigger buttotn

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ const App = () => {
 
   return (
     <div style={containerStyle}>
-      <button onClick={() => {
+      {/* <button onClick={() => {
         const testMergeConfig = JSON.stringify({
           token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYTQzNWNlNDM2NjQ5Y2FmYzE1ZDhhNGY4YTJmYjM1ZjY3YmFiOGNlNGFhZWE0MmVjYzVlOTUwNzIxNjhiNGY2N2NiZDYxZTZkZmU3MGQ2ZTIiLCJpYXQiOjE3NTQzODQ4OTguMzQ4Nzk3LCJuYmYiOjE3NTQzODQ4OTguMzQ4Nzk4LCJleHAiOjE3NTQ2NDQwOTguMzM1ODU0LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.YMuhmWnGdbQriWvt_EkdZVx7sGZ7fF3Z7kuZMHOClgdWMms-yiCPbqEg-zX0t0V3y1_VrlfEafl3mzs2OBcGDCP0h_BnXjbhNaWaQX_1If13WLqXcIt7LwUzLw_SbPEphvDj4B1P6RqNV_yZTU3JuFOzEpj7VUHscbQiCV_Vabe3TasyuSJM8gcsHSG7sQobrSD_cL8lD0B81Pc6XUys8g7HJI62wwB3H-s-Axhxbf7JDPa3cdhkbtypPI08n-QQX3qBwiT0vdg9WLTP7vdkEMFCfK4KD6YMZt44aCdnAlPK5n63RFhi983Im2kCGN2D_AqpccRbxhwaYwrgSTQwMNOGZVAuZ3PzAjgZhRy5IVG2EpA8krZw4lDSCsCL_3-LdJOAAu1a2Y6EG4yTgqDyBI_FlKWti4T-FecXCSl_OK7kjREdLzHCkwggA5uGM7KlaC2NXLwRbG4vCDSg3wyOoTvYvrFaDrqcibgx7rZrjkk5ViKa5EUYQ9_47ZyyD9Ff6xMCxRac1XXeSqXfKh08whwUOwPlx5_yhiynfcBA2-TX25ajqZJ0bTH6ParX6viky4_gWBszk-5U2fsqgOK1Z-iXwUjvrpKIbGeluU3qF9p6MU90C9NK1aoIAo0Za-csZqa8DCIPw6N4A5ehw5SzDCdMMVg6XfQoZuL9tvdP3lA",
           mode: "single",
@@ -119,7 +119,7 @@ const App = () => {
         window.receiveMessageFromFlutter?.(testMergeConfig);
       }}>
         🔄 Trigger Merge Mode Test
-      </button>
+      </button> */}
 
       {/* <h2>Family Tree Viewer</h2> */}
 
